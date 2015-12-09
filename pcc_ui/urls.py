66c16a858e079b47c623abe1c67aebb62974281b
goldsoft1206 <icbsoft87@gmail.com>
2015-12-09 20:55:32 +0800

    renamed ppc to pcc

--- pcc_ui/urls.py
+++ pcc_ui/urls.py
@@ -1,4 +1,4 @@
-"""ppc_ui URL Configuration
+"""pcc_ui URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
