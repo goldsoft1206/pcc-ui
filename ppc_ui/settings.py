79bf665f9f33845d8dd6ef48f7d5df71cfe4fd42
goldsoft1206 <icbsoft87@gmail.com>
2015-12-09 20:42:33 +0800

    updated STATICFILES_DIRS and TEMPLATES_DIRS

--- ppc_ui/settings.py
+++ ppc_ui/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ppc_ui.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,6 +103,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # django-grappelli related settings
